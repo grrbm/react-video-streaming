c0ba63828dd02c6e7dd9775cd786d349148c266e
Guilherme Reis <grrbm2@gmail.com>
2021-11-20 14:57:13 -0300

    added sass

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,7 @@
 module.exports = {
   //configure the purge option with the paths to all of your components
   //so Tailwind can tree-shake unused styles in production builds:
-  purge: [
-    "./**/*.{js, jsx,ts,tsx,css}",
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./public/index.html",
-  ],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
     extend: {},
   },
