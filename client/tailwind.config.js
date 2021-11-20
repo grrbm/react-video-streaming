5d3f6e3913fa244068f5ba413e2de601c7603845
Guilherme Reis <grrbm2@gmail.com>
2021-11-20 16:02:14 -0300

    added mode "jit"

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   //configure the purge option with the paths to all of your components
   //so Tailwind can tree-shake unused styles in production builds:
+  mode: "jit",
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
     extend: {},
