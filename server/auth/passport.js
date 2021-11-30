e715100b6c47046611ca4a46f118161128d4fbd4
Guilherme Reis <grrbm2@gmail.com>
2021-11-30 20:43:15 -0300

    fixed missing credentials error

--- server/auth/passport.js
+++ server/auth/passport.js
@@ -1,23 +1,31 @@
 const passport = require("passport"),
   LocalStrategy = require("passport-local").Strategy,
   User = require("../database/Schema").User,
-  shortid = require("shortid");
+  shortid = require("shortid"),
+  bcrypt = require("bcryptjs");
 
 passport.use(
-  new LocalStrategy((username, password, done) => {
-    User.findOne({ username: username }, (err, user) => {
-      if (err) throw err;
-      if (!user) return done(null, false);
-      bcrypt.compare(password, user.password, (err, result) => {
+  new LocalStrategy(
+    {
+      usernameField: "email", // define the parameter in req.body that passport can use as username and password
+      passwordField: "password",
+      passReqToCallback: true,
+    },
+    (req, email, password, done) => {
+      User.findOne({ email: email }, (err, user) => {
         if (err) throw err;
-        if (result === true) {
-          return done(null, user);
-        } else {
-          return done(null, false);
-        }
+        if (!user) return done(null, false);
+        bcrypt.compare(password, user.password, (err, result) => {
+          if (err) throw err;
+          if (result === true) {
+            return done(null, user);
+          } else {
+            return done(null, false);
+          }
+        });
       });
-    });
-  })
+    }
+  )
 );
 
 passport.serializeUser((user, cb) => {
