56362285960584944a053eeb63c428f4f5fb0ee9
Guilherme Reis <grrbm2@gmail.com>
2021-11-30 18:39:19 -0300

    isolated register and login routes

--- server/routes/login.js
+++ server/routes/login.js
@@ -2,7 +2,7 @@ const express = require("express"),
   router = express.Router(),
   passport = require("passport");
 
-router.post("/login", (req, res, next) => {
+router.post("/", (req, res, next) => {
   passport.authenticate("local", (err, user, info) => {
     if (err) throw err;
     if (!user) res.send("No User Exists");
@@ -14,22 +14,6 @@ router.post("/login", (req, res, next) => {
       });
     }
   })(req, res, next);
-});
-router.post("/register", (req, res) => {
-  User.findOne({ username: req.body.username }, async (err, doc) => {
-    if (err) throw err;
-    if (doc) res.send("User Already Exists");
-    if (!doc) {
-      const hashedPassword = await bcrypt.hash(req.body.password, 10);
-
-      const newUser = new User({
-        username: req.body.username,
-        password: hashedPassword,
-      });
-      await newUser.save();
-      res.send("User Created");
-    }
-  });
 });
 
 module.exports = router;
--- server/routes/register.js
+++ server/routes/register.js
@@ -2,28 +2,20 @@ const express = require("express"),
   router = express.Router(),
   passport = require("passport");
 
-router.get(
-  "/",
-  require("connect-ensure-login").ensureLoggedOut(),
-  (req, res) => {
-    res.render("register", {
-      user: null,
-      errors: {
-        username: req.flash("username"),
-        email: req.flash("email"),
-      },
-    });
-  }
-);
+router.post("/", (req, res) => {
+  User.findOne({ username: req.body.username }, async (err, doc) => {
+    if (err) throw err;
+    if (doc) res.send("User Already Exists");
+    if (!doc) {
+      const hashedPassword = await bcrypt.hash(req.body.password, 10);
 
-router.post(
-  "/",
-  require("connect-ensure-login").ensureLoggedOut(),
-  passport.authenticate("localRegister", {
-    successRedirect: "/",
-    failureRedirect: "/register",
-    failureFlash: true,
-  })
-);
-
+      const newUser = new User({
+        username: req.body.username,
+        password: hashedPassword,
+      });
+      await newUser.save();
+      res.send("User Created");
+    }
+  });
+});
 module.exports = router;
