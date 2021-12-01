9a767e23f21368426ccb30aa70f03ec20ee6475f
Guilherme Reis <grrbm2@gmail.com>
2021-12-01 19:17:02 -0300

    changed config file

---
+++ server/config/default.js
@@ -0,0 +1,36 @@
+const config = {
+  mongodbURL: "mongodb://localhost:27017/ReactVideoStreaming",
+  secret_token: "1b671a64-40d5-491e-99b0-da01ff1f3341",
+  server: {
+    secret: "ASOlojaga812imsxaofdApldgosfmcv891kKSkdf",
+    port: 3333,
+  },
+  rtmp_server: {
+    rtmp: {
+      port: 1935,
+      chunk_size: 60000,
+      gop_cache: true,
+      ping: 60,
+      ping_timeout: 30,
+    },
+    http: {
+      port: 8888,
+      mediaroot: "./media",
+      allow_origin: "*",
+    },
+    trans: {
+      ffmpeg:
+        process.env.FFMPEG_PATH || "C:/ProgramData/chocolatey/bin/ffmpeg.exe",
+      tasks: [
+        {
+          app: "live",
+          hls: true,
+          hlsFlags: "[hls_time=2:hls_list_size=3:hls_flags=delete_segments]",
+          dash: true,
+          dashFlags: "[f=dash:window_size=3:extra_window_size=5]",
+        },
+      ],
+    },
+  },
+};
+module.exports = config;
