a056ab33d4854a43900336cb79473402a021f3a9
Guilherme Reis <grrbm2@gmail.com>
2021-12-01 17:42:10 -0300

    added css for video player

--- client/src/components/LiveStreamPlayer/index.js
+++ client/src/components/LiveStreamPlayer/index.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useRef } from "react";
 import videojs from "video.js";
 import axios from "axios";
 import config from "../../config/default";
+import "./LiveStreamPlayer.scss";
 
 export default function LiveStreamPlayer(props) {
   const [stream, setStream] = useState(false);
