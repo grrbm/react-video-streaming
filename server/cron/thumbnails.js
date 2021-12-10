641e32307f4b09cdd0ae69b89e69c226b0107d02
Guilherme Reis <grrbm2@gmail.com>
2021-12-10 17:06:41 -0300

    printing full path

--- server/cron/thumbnails.js
+++ server/cron/thumbnails.js
@@ -15,23 +15,22 @@ if (environment === "development") {
 const job = new CronJob(
   "*/5 * * * * *",
   function () {
-    request.get(
-      `http://${baseUrl}:` + port + "/api/streams",
-      function (error, response, body) {
-        if (error) {
-          console.log("There was an error fetching thumbnail: " + error);
-          return;
-        }
-        let streams = JSON.parse(body);
-        if (typeof (streams["live"] !== undefined)) {
-          let live_streams = streams["live"];
-          for (let stream in live_streams) {
-            if (!live_streams.hasOwnProperty(stream)) continue;
-            helpers.generateStreamThumbnail(stream);
-          }
+    const fullUrl = `http://${baseUrl}:` + port + "/api/streams";
+    request.get(fullUrl, function (error, response, body) {
+      if (error) {
+        console.log("There was an error fetching thumbnail: " + error);
+        console.log("Tried to fetch: " + fullUrl);
+        return;
+      }
+      let streams = JSON.parse(body);
+      if (typeof (streams["live"] !== undefined)) {
+        let live_streams = streams["live"];
+        for (let stream in live_streams) {
+          if (!live_streams.hasOwnProperty(stream)) continue;
+          helpers.generateStreamThumbnail(stream);
         }
       }
-    );
+    });
   },
   null,
   true
