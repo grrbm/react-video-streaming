fc9c0cc5705bd9e4fd10ac3d7b724cedbf73a3ed
Guilherme Reis <grrbm2@gmail.com>
2021-12-10 16:37:18 -0300

    attempt fix thumbnail error

--- server/cron/thumbnails.js
+++ server/cron/thumbnails.js
@@ -4,13 +4,24 @@ const CronJob = require("cron").CronJob,
   config = require("../config/default"),
   port = config.rtmp_server.http.port;
 
-const baseUrl = process.env.STREAMINGVARIABLE || "127.0.0.1";
+const environment = process.env.NODE_ENV;
+let baseUrl;
+if (environment === "development") {
+  baseUrl = "http://127.0.0.1";
+} else {
+  //if it's in production
+  baseUrl = config.productionUrl;
+}
 const job = new CronJob(
   "*/5 * * * * *",
   function () {
     request.get(
       `http://${baseUrl}:` + port + "/api/streams",
       function (error, response, body) {
+        if (error) {
+          console.log("There was an error fetching thumbnail: " + error);
+          return;
+        }
         let streams = JSON.parse(body);
         if (typeof (streams["live"] !== undefined)) {
           let live_streams = streams["live"];
