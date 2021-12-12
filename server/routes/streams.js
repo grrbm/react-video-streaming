d0faf174b2e7218f5735e8f2849216071154c618
Guilherme Reis <grrbm2@gmail.com>
2021-12-12 14:18:28 -0300

    created function to get list of streams

--- server/routes/streams.js
+++ server/routes/streams.js
@@ -1,6 +1,16 @@
 const express = require("express"),
   router = express.Router(),
-  User = require("../database/Schema").User;
+  User = require("../database/Schema").User,
+  axios = require("axios");
+
+const environment = process.env.NODE_ENV;
+let baseUrl;
+if (environment === "production") {
+  //if it's in production
+  baseUrl = "http://localhost";
+} else {
+  baseUrl = "http://127.0.0.1";
+}
 
 router.get("/info", (req, res) => {
   if (req.query.streams) {
@@ -19,4 +29,16 @@ router.get("/info", (req, res) => {
     });
   }
 });
+
+router.get("/streams/list", async (req, res) => {
+  try {
+    let fullUrl = `${baseUrl}:` + port + "/api/streams";
+    fullUrl = fullUrl.replace(/https/, "http");
+    const result = axios.get(fullUrl);
+    res.status(200).send(result.data);
+  } catch (error) {
+    res.status(500).send("Error getting list of streams available.");
+  }
+});
+
 module.exports = router;
