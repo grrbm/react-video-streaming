a13ace8c4ef4c05ef0d2da629ed2645aa56b26ae
Guilherme Reis <grrbm2@gmail.com>
2021-12-12 16:24:51 -0300

    setup proxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    "/live/*/*",
+    "https://loud-now.cyou:8888",
     createProxyMiddleware({
       target: "http://server:8888",
       changeOrigin: true,
