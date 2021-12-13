1ae8e2704d389884bdb3068b2eede1611cfd7814
Guilherme Reis <grrbm2@gmail.com>
2021-12-12 22:00:01 -0300

    replaced the url

--- server/cron/thumbnails.js
+++ server/cron/thumbnails.js
@@ -16,6 +16,7 @@ const job = new CronJob(
   "*/5 * * * * *",
   function () {
     let fullUrl = `${baseUrl}:` + port + "/api/streams";
+    fullUrl = fullUrl.replace(/https/, "http");
     request.get(fullUrl, function (error, response, body) {
       if (error) {
         console.log("There was an error fetching thumbnail: " + error);
